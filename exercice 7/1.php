f4aad603341e790ffea18b1ccaf1032ab6d015cd
cassebonbon <claddupont9@gmail.com>
2023-10-27 11:57:45 +0200

    commentaire

---
+++ exercice 7/1.php
@@ -0,0 +1,98 @@
+<?php
+// 
+echo "exo 1" ."\n";
+
+// $n = readline("entrez la taille du tableau ? ");
+//  for ($i = 0; $i < $n; $i++) {
+//  $tab[$i] = readline("Entrez la valeur numéro " .   " : ") ;
+// }
+
+// echo "Saisissez les valeurs une par une (saisie vide pour terminer) :";
+
+// $tab = array();
+// while (true) {
+//     $input = readline("> ");
+    
+//     if ($input === "") {
+//         break;
+//     }
+    
+//     $tab[] = (int)$input;
+// }
+// $estVrai = true;
+// while($estVrai){
+// $estVrai = false;
+// for($i =0;$i<=count($tab)-2;$i++){
+// if($tab[$i]>$tab[$i+1]){
+// $temp = $tab[$i];
+// $tab[$i]=$tab[$i+1];
+// $tab[$i+1] =$temp ;
+// $estVrai = true;
+// }   
+// }
+// }
+// $isConsecutive = true;
+// $length = count($tab);
+// if ($length > 1) {
+//     sort($tab);
+//     for ($i = 0; $i < $length - 1; $i++) {
+//         if ($tab[$i] + 1 !== $tab[$i+1]) {
+//             $isConsecutive = false;
+//             break;
+//         }
+//     }
+// }
+// if ($isConsecutive) {
+//     echo "Les éléments du tableau sont tous consécutifs.";
+// } else {
+//     echo "Les éléments du tableau ne sont pas tous consécutifs.";
+// }
+
+
+
+
+$tableau = [];
+
+// Saisie des valeurs
+while (true) {
+    $valeur = readline("Saisir une valeur (ou 'q' pour terminer) : ");
+    
+ 
+    if ($valeur === 'q') {
+        break;
+    }
+    
+
+    $tableau[] = intval($valeur);
+}
+//  $estVrai = true;
+//  while($estVrai){
+//      $estVrai = false;
+//  for($i =0;$i<=count($tab)-2;$i++){
+//  if($tab[$i]>$tab[$i+1]){
+//  $temp = $tab[$i];
+//  $tab[$i]=$tab[$i+1];
+//  $tab[$i+1] =$temp ;
+//  $estVrai = true;
+//  }   
+ 
+$isConsecutif = true;
+$n = count($tableau);
+
+
+for ($i = 0; $i < $n - 1; $i++) {
+  
+    if ($tableau[$i+1] - $tableau[$i] !== 1) {
+        $isConsecutif = false;
+        break;
+    }
+}
+
+
+if ($isConsecutif) {
+    echo "Les éléments du tableau sont tous consécutifs.";
+} else {
+    echo "Les éléments du tableau ne sont pas tous consécutifs.";
+}
+
+?>
---
+++ exercice 7/entrainement.php
@@ -0,0 +1,45 @@
+<?php
+// 
+echo "exo 1" ."\n";
+
+// $notes = [];
+// $count = (int)readline("Combien de notes souhaitez-vous saisir ? ");
+// $nombre = readline("entrez vos nombre ? ");
+// for ($i = 1; $i <= $count; $i++) ;
+
+
+
+
+ $n = readline("entrez la taille du tableau ? ");
+ for ($i = 0; $i < $n; $i++) {
+ $tab[$i] = readline("Entrez la valeur numéro " .   " : ") ;
+}
+$estVrai = true;
+while($estVrai){
+$estVrai = false;
+for($i =0;$i<=count($tab)-2;$i++){
+if($tab[$i]>$tab[$i+1]){
+$temp = $tab[$i];
+$tab[$i]=$tab[$i+1];
+$tab[$i+1] =$temp ;
+$estVrai = true;
+}
+ 
+   
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+foreach($tab as $valeur){
+echo $valeur . "\n";
+
+}
